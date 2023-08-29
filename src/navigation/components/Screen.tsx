49d7a5ba6fbbeff4443ed545896ac03182705862
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-08-29 14:04:52 +0900

    Fix: 삭제-Screen.tsx / title, name eslint 경고

--- src/navigation/components/Screen.tsx
+++ src/navigation/components/Screen.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export interface ScreenProps {
   component: React.ComponentType;
   name: string;
   title: string;
 }
 
+// name, title 프로퍼티는 Navigator 에서 사용함
 const Screen = ({
   component: Component,
   name,
